4da9a6d458e69baada499c7003e85800e59834e1
cdivagar <cdivagar4@gmail.com>
2025-04-04 19:24:16 +0530

    Simple Calculator Program

---
+++ Day7CalculatorProject/MainWindow.xaml.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Day7CalculatorProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        String firstnum;
+        String secondnum;
+        String oper;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            Button operators = (Button)sender;
+            oper = operators.Content.ToString();
+        }
+
+        //private void btnsub_Click(object sender, RoutedEventArgs e)
+        //{
+        //    oper = btnsub.Content.ToString();
+        //}
+
+        //private void btnmul_Click(object sender, RoutedEventArgs e)
+        //{
+        //    oper = btnmul.Content.ToString();
+        //}
+
+        private void btneq_Click(object sender, RoutedEventArgs e)
+        {
+            int first = Convert.ToInt16(firstnum);
+            int second = Convert.ToInt16(secondnum);
+            int result = 0;
+            switch (oper)
+            {
+                case "+":
+                    {
+                        result = first + second;
+                        break;
+                    }
+                case "-":
+                    {
+                        result = first - second;
+                        break;
+                    }
+                case "*":
+                    {
+                        result = first * second;
+                        break;
+                    }
+                case "/":
+                    {
+                        result = first / second;
+                        break;
+
+                    }
+            }
+            resultbox.Text = result.ToString();
+        }
+     
+
+        private void btnc_Click(object sender, RoutedEventArgs e)
+        {
+            firstnum = "";
+            secondnum = "";
+            resultbox.Clear();
+            oper = "";
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            Button num = (Button)sender;
+            if (oper == "")
+            {
+                firstnum = firstnum + num.Content.ToString();
+                resultbox.Text = firstnum;
+            }
+            else
+            {
+                secondnum = secondnum + num.Content.ToString();
+                resultbox.Text = secondnum;
+            }
+        }
+
+        //private void btn2_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (oper == "")
+        //    {
+        //        firstnum = firstnum + btn2.Content.ToString();
+        //        resultbox.Text = firstnum;
+        //    }
+        //    else
+        //    {
+        //        secondnum = secondnum + btn2.Content.ToString();
+        //        resultbox.Text = secondnum;
+        //    }
+        //}
+
+        //private void btn3_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (oper == "")
+        //    {
+        //        firstnum = firstnum + btn3.Content.ToString();
+        //        resultbox.Text = firstnum;
+        //    }
+        //    else
+        //    {
+        //        secondnum = secondnum + btn3.Content.ToString();
+        //        resultbox.Text = secondnum;
+        //    }
+        //}
+
+        //private void btn7_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (oper == "")
+        //    {
+        //        firstnum = firstnum + btn7.Content.ToString();
+        //        resultbox.Text = firstnum;
+        //    }
+        //    else
+        //    {
+        //        secondnum = secondnum + btn7.Content.ToString();
+        //        resultbox.Text = secondnum;
+        //    }
+        //}
+
+        //private void btn8_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (oper == "")
+        //    {
+        //        firstnum = firstnum + btn8.Content.ToString();
+        //        resultbox.Text = firstnum;
+        //    }
+        //    else
+        //    {
+        //        secondnum = secondnum + btn8.Content.ToString();
+        //        resultbox.Text = secondnum;
+        //    }
+        //}
+
+        //private void btn9_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (oper == "")
+        //    {
+        //        firstnum = firstnum + btn9.Content.ToString();
+        //        resultbox.Text = firstnum;
+        //    }
+        //    else
+        //    {
+        //        secondnum = secondnum + btn9.Content.ToString();
+        //        resultbox.Text = secondnum;
+        //    }
+        //}
+
+        //private void btn4_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (oper == "")
+        //    {
+        //        firstnum = firstnum + btn4.Content.ToString();
+        //        resultbox.Text = firstnum;
+        //    }
+        //    else
+        //    {
+        //        secondnum = secondnum + btn4.Content.ToString();
+        //        resultbox.Text = secondnum;
+        //    }
+        //}
+
+        //private void btn5_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (oper == "")
+        //    {
+        //        firstnum = firstnum + btn5.Content.ToString();
+        //        resultbox.Text = firstnum;
+        //    }
+        //    else
+        //    {
+        //        secondnum = secondnum + btn5.Content.ToString();
+        //        resultbox.Text = secondnum;
+        //    }
+        //}
+
+        //private void btn6_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (oper == "")
+        //    {
+        //        firstnum = firstnum + btn6.Content.ToString();
+        //        resultbox.Text = firstnum;
+        //    }
+        //    else
+        //    {
+        //        secondnum = secondnum + btn6.Content.ToString();
+        //        resultbox.Text = secondnum;
+        //    }
+        //}
+
+        ////private void btndiv_Click(object sender, RoutedEventArgs e)
+        ////{
+        ////    oper = btndiv.Content.ToString();
+        ////}
+
+        //private void btn00_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (oper == "")
+        //    {
+        //        firstnum = firstnum + btn00.Content.ToString();
+        //        resultbox.Text = firstnum;
+        //    }
+        //    else
+        //    {
+        //        secondnum = secondnum + btn00.Content.ToString();
+        //        resultbox.Text = secondnum;
+        //    }
+        //}
+
+        //private void btnsub_Click(object sender, RoutedEventArgs e)
+        //{
+
+        //}
+    }
+}
---
+++ Day7CalculatorProject/login.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Day7CalculatorProject
+{
+    /// <summary>
+    /// Interaction logic for login.xaml
+    /// </summary>
+    public partial class login : Window
+    {
+        string uid = "Admin";
+        string pwd = "Admin123";
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            //method-4:(to check empty space )
+            //if (txtusername.Text == "" || txtpassword.Password == "")
+            //{
+            //    MessageBox.Show("please check the username and password");
+            //}
+
+            //method-5:(to check the null & white space using [IsNullOrWhiteSpace] function) 
+            if(string.IsNullOrWhiteSpace(txtusername.Text)==true || string.IsNullOrWhiteSpace(txtpassword.Password)==true)
+            {
+                MessageBox.Show("please check the user and password");
+            }
+            else
+            {
+                //method-3:(to  check the condition in one line)
+
+                if (txtusername.Text == uid && txtpassword.Password == pwd)
+                {
+                    MessageBox.Show("welcome","jsquare",MessageBoxButton.OKCancel,MessageBoxImage.Exclamation);
+                }
+                else
+                {
+                    MessageBox.Show("username and password invalid");
+                }
+            }
+            //method-2:
+            //if (txtusername.Text == uid)
+            //{
+            //    if (txtpassword.Password == pwd)
+            //    {
+            //        MessageBox.Show("welcome");
+
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("password invalid");
+            //    }
+
+            //}
+            //else
+            //{
+            //    MessageBox.Show("please enter correct username and password");
+            //}
+         //method-1
+         
+        //    if(txtusername.Text==txtpassword.Password)
+        //    {
+        //        MessageBox.Show("welcome");
+        //    }
+        //    if(txtusername.Text != txtpassword.Password)
+        //    {
+        //        MessageBox.Show("Password invalid");
+        //    }
+        }
+    }
+}
